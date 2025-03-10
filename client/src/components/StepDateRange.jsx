85a72d6304d6a37614c3f28f529eb8754fceeeba
Atul Yadav <atul18542@gmail.com>
2025-03-10 17:26:27 +0530

    Implement booking feature with GraphQL integration and update UI components

--- client/src/components/StepDateRange.jsx
+++ client/src/components/StepDateRange.jsx
@@ -1,15 +1,29 @@
-import { Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { useState } from "react";
+import { gql, useMutation } from "urql";
+
+const MUTATION_CREATE_BOOKING = gql`
+  mutation CreateBooking($createBookingInput: CreateBookingInput!) {
+    createBooking(createBookingInput: $createBookingInput) {
+      id
+      startDate
+      endDate
+    }
+  }
+`;
 
 const StepDateRange = ({ formData, setFormData, onSuccess }) => {
   const [inputError, setInputError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [, createBooking] = useMutation(MUTATION_CREATE_BOOKING);
 
   const handleInputChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     let newError = {};
 
@@ -32,13 +46,51 @@ const StepDateRange = ({ formData, setFormData, onSuccess }) => {
 
     setInputError(newError);
 
-    if (Object.keys(newError).length === 0) {
+    if (!(Object.keys(newError).length === 0)) {
+      setIsLoading(false);
+      return;
+    }
+
+    const { data } = await createBooking({
+      createBookingInput: {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        startDate: formData.startDate,
+        endDate: formData.endDate,
+        vehicleId: formData.specificModel,
+      },
+    });
+
+    if (data) {
+      console.log(data?.createBooking?.id);
+
+      handleInputChange("bookingId", data?.createBooking?.id || "");
+      setIsLoading(false);
       onSuccess();
     }
+    setIsLoading(false);
   };
 
   return (
     <>
+      {isLoading && (
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            zIndex: 9999,
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
       <form id="dateRange-form" onSubmit={handleSubmit} />
       <Typography variant="h5">Select your booking duration</Typography>
       <DatePicker
--- client/src/components/StepSuccess.jsx
+++ client/src/components/StepSuccess.jsx
@@ -1,5 +1,109 @@
-const StepSuccess = () => {
-  return <div>StepSuccess</div>;
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { gql, useQuery } from "urql";
+import dayjs from "dayjs";
+
+const QUERY_BOOKING_DETAILS = gql`
+  query Booking($bookingId: String!) {
+    booking(id: $bookingId) {
+      id
+      startDate
+      endDate
+      user {
+        id
+        firstName
+        lastName
+      }
+      vehicle {
+        id
+        isAvailable
+        name
+        vehicleTypeId
+      }
+    }
+  }
+`;
+
+const StepSuccess = ({ formData }) => {
+  const [bookDetails, setBookingDetails] = useState({
+    firstName: "Loading...",
+    lastName: "Loading...",
+    specificModel: "Loading...",
+    startDate: "Loading...",
+    endDate: "Loading...",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [bookingDetails] = useQuery({
+    query: QUERY_BOOKING_DETAILS,
+    variables: {
+      bookingId: formData?.bookingId,
+    },
+  });
+
+  const { data, fetching, error } = bookingDetails;
+  useEffect(() => {
+    if (!fetching) {
+      setIsLoading(false);
+    }
+    if (data?.booking) {
+      setBookingDetails({
+        firstName: data?.booking?.user?.firstName,
+        lastName: data?.booking?.user?.lastName,
+        specificModel: data?.booking?.vehicle?.name,
+        startDate: dayjs(data?.booking?.startDate).format("MM-DD-YYYY"),
+        endDate: dayjs(data?.booking?.endDate).format("MM-DD-YYYY"),
+      });
+    }
+  }, [data?.booking, fetching]);
+
+  useEffect(() => {
+    console.log(formData?.bookingId);
+  });
+
+  return (
+    <>
+      {isLoading && (
+        <Box
+          sx={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            zIndex: 9999,
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+      {error && <Typography variant="h5">Error Occured!</Typography>}
+      {data && (
+        <>
+          <Typography variant="h5">Sucesfully Booked!</Typography>
+          <Typography variant="subtitle1">
+            First Name : {bookDetails.firstName}
+          </Typography>
+          <Typography variant="subtitle1">
+            Last Name : {bookDetails.lastName}
+          </Typography>
+          <Typography variant="subtitle1">
+            Vehicle Model : {bookDetails.specificModel}
+          </Typography>
+          <Typography variant="subtitle1">
+            Start Date (MM-DD-YYYY) : {bookDetails.startDate}
+          </Typography>
+          <Typography variant="subtitle1">
+            End Date (MM-DD-YYYY) : {bookDetails.endDate}
+          </Typography>
+        </>
+      )}
+    </>
+  );
 };
 
 export default StepSuccess;
